751a185e6d6359740966828d939f719ff0ff5d86
Sandeep Khachariya <2023ucp1628@mnit.ac.in>
2024-10-17 11:51:20 +0530

    STASH APPLY

---
+++ prod.py
@@ -0,0 +1,16 @@
+a=[]
+b=[]
+n=int(input("Enter no. of elements : "))
+for i in range(0,n,1):
+      x=int(input("Enter elements : "))
+      a.append(x)
+print(a)
+for j in range(0,n,1):
+      y=int(input("Enter elements : "))
+      b.append(y)
+print(b)
+
+r=0
+for i in range(0,n,1):
+      r += a[i]*b[i]
+print(r)
